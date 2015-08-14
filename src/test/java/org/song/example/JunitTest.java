39c102f9fb0616df06fead6d87492e096cd4401d
Sky Liu <sliu@guidewire.com>
2015-08-14 15:42:05 -0700

    initial commit

---
+++ src/test/java/org/song/example/JunitTest.java
@@ -0,0 +1,14 @@
+package org.song.example;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+
+/**
+ * Created by sliu on 8/14/2015.
+ */
+public class JunitTest {
+//    @Test
+//    public void testMethod2(){
+//        Assert.assertEquals(3, 3);
+//    }
+}
