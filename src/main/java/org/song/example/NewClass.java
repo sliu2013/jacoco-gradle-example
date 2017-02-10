72db63b2c49ff9cc70cb67acd12647ede6339133
Sky Liu <sliu@guidewire.com>
2017-02-10 13:40:54 -0800

    test codecov.io

---
+++ src/main/java/org/song/example/NewClass.java
@@ -0,0 +1,13 @@
+package org.song.example;
+
+/**
+ * Created by sliu on 2/10/2017.
+ */
+public class NewClass {
+  public void missingCov1(){
+    int x = 10;
+    x = x + 100;
+    x = x / 30;
+  }
+
+}
